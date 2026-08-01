56758626acdbb3473d2fbc24ad061e77ba7cc322
Nishtha-agarwal <93310872+Nishtha-agarwal@users.noreply.github.com>
2026-08-01 14:19:30 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,28 +7,21 @@ const API = axios.create({
 export const uploadFile = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
-
   const response = await API.post("/upload/", formData);
 
   console.log("=== UPLOAD API ===");
   console.log("Axios response:", response);
   console.log("Response data:", response.data);
-  console.log("Response path:", response.data?.path);
-
   return response.data;
 };
 
 export const processDocument = async (path) => {
   console.log("=== PROCESS API ===");
   console.log("Sending path:", path);
-
   const response = await API.post("/process/", {
     path: path,
   });
-
-  console.log("Process response:", response);
   console.log("Process data:", response.data);
-
   return response.data;
 };
 
