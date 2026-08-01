a6dbff3a94a2b03917899a3f68165cc584285885
Nishtha-agarwal <93310872+Nishtha-agarwal@users.noreply.github.com>
2026-08-01 12:27:24 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://teacher-ai-platform.onrender.com",
+  baseURL: "import.meta.env.VITE_API_URL,
 });
 
 export const uploadFile = (file) => {
